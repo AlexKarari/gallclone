54c8a0c75f2d0222e94da9f5ccae14abd591fc9c
alexkarari <k.alexander@gmail.com>
2018-05-06 17:20:01 +0300

    Tests
    Added tests

--- pics/tests.py
+++ pics/tests.py
@@ -1,3 +1,66 @@
 from django.test import TestCase
+import datetime as dt
+from .models import Location, Category, Image
 
 # Create your tests here.
+
+
+class LocationTestClass(TestCase):
+    def setUp(self):
+        self.location = Location(name='Nairobi')
+
+    def test_instance(self):
+        self.assertTrue(isinstance(self.location, Location))
+
+    def test_save_method(self):
+        self.location.save_location()
+        locations = Location.objects.all()
+        self.assertTrue(len(locations) > 0)
+
+    def test_delete_method(self):
+        self.location.save_location()
+        locations = Location.objects.all()
+        self.location.delete_location()
+        locations = Location.objects.all()
+        self.assertTrue(len(locations) == 0)
+
+
+class CategoryTestClass(TestCase):
+    def setUp(self):
+        self.category = category(name='science')
+
+    def test_category_instance(self):
+        self.assertTrue(isinstance(self.category, Category))
+
+    def test_save_category_method(self):
+        self.category.save_category()
+        category_object = category.objects.all()
+        self.assertTrue(len(category_object) > 0)
+
+    def test_delete_category_method(self):
+        self.category.save_category()
+        category_object = Category.objects.all()
+        self.category.delete_category()
+        category_object = Category.objects.all()
+        self.assertTrue(len(category_object) == 0)
+
+
+class ImageTestClass(TestCase):
+    def setUp(self):
+        self.image = Image(image='imageurl', image_name='nature', image_description='the beautiful nature')
+
+    def test_image_instance(self):
+        self.assertTrue(isinstance(self.image, Image))
+
+    def test_save_image_method(self):
+        self.image.save_image()
+        Image = Image.objects.all()
+        self.assertTrue(len(Image) > 0)
+
+    def test_delete_image_method(self):
+        self.image.save_image()
+        Image = Image.objects.all()
+        self.image.delete_image()
+        Image = Image.objects.all()
+        self.assertTrue(len(Image) == 0)
+
